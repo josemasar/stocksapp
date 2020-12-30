e563d10fe98b03bc4dcc14971dfdef09f2639e3b
josemasar <josemanuelsarrion@gmail.com>
2020-12-30 13:16:07 +0100

    added price and rsi char and hidden apikey token

--- src/components/ApiConnectorRSI.js
+++ src/components/ApiConnectorRSI.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import localForage from 'localforage'
 import { setupCache } from 'axios-cache-adapter'
+import apikey from './config'
 
 const cache = setupCache({
     maxAge: 60* 60 * 1000,
@@ -23,7 +24,7 @@ export const getRSIForSymbol = (symbol) => {
             interval : 'weekly',
             time_period : '60',
             series_type : 'open',
-            apikey: '1OQ7RJTF331B5VFA'
+            apikey
             }
         }
     )
